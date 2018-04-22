07d848b7662001db97578dbab42e9c77ea80b033
Purva Bundela <Panchi@Purvas-MacBook-Air.local>
2018-04-22 12:52:08 -0400

    adding twitt

--- Stock-Market-Prediction/app/controllers/HomeController.scala
+++ Stock-Market-Prediction/app/controllers/HomeController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject.Inject
+
 import actors.{LoginActor, StockActor}
 import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
@@ -15,6 +16,7 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.JsValue
 import play.api.libs.streams.ActorFlow
 import play.api.mvc._
+import views.{html, _}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -43,49 +45,49 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents)(u
     Ok(views.html.index(FormsData.userForm)(FormsData.createUserForm)(StringUtils.EMPTY))
   }
 
-//    val stockRouter = system.actorOf(StockActor.props,"Test")
-//    implicit val timeout: Timeout = 30.seconds
-//  def userLogin = Action.async {
-//      implicit request =>
-//          Supervision.Restart
-//         println("userlogin")
-//      (stockRouter ? getStocks("abc")).mapTo[Double].map{
-//          abc => Ok(views.html.loggedInPage(abc))
-//      }
-////
-////
-//////
-//////
-////      FormsData.userForm.bindFromRequest().fold(
-////        formWithErrors => Future.successful(BadRequest),
-////          abc => {
-////              stockRouter ? StockActor.Stock("abc")
-////          }.mapTo[String].map {
-////              Ok(views.html.loggedInPage(abc))
-////////          case Some(user) =>
-////////          case None => Ok("Invalid credentials")
-//////        }
-////      )
-//  }
-    val abc = Util.Timeseries.trainAndPredictPrice()
+  //    val stockRouter = system.actorOf(StockActor.props,"Test")
+  //    implicit val timeout: Timeout = 30.seconds
+  //  def userLogin = Action.async {
+  //      implicit request =>
+  //          Supervision.Restart
+  //         println("userlogin")
+  //      (stockRouter ? getStocks("abc")).mapTo[Double].map{
+  //          abc => Ok(views.html.loggedInPage(abc))
+  //      }
+  ////
+  ////
+  //////
+  //////
+  ////      FormsData.userForm.bindFromRequest().fold(
+  ////        formWithErrors => Future.successful(BadRequest),
+  ////          abc => {
+  ////              stockRouter ? StockActor.Stock("abc")
+  ////          }.mapTo[String].map {
+  ////              Ok(views.html.loggedInPage(abc))
+  ////////          case Some(user) =>
+  ////////          case None => Ok("Invalid credentials")
+  //////        }
+  ////      )
+  //  }
+  val abc = Util.Timeseries.trainAndPredictPrice()
 
-    def userLogin = Action.async {
-        implicit request =>
-            implicit val timeout: Timeout = Timeout(2 seconds)
-            FormsData.userForm.bindFromRequest().fold(
-                formWithErrors => Future.successful(BadRequest),
-                userTuple => {
-                    loginRouter ? LoginActor.GetUser(userTuple._1, userTuple._2)
-                } map {
-                    case Some(user) => Ok(views.html.loggedInPage(abc)).withSession("user" -> userTuple._1)
-                    case None => Ok("Invalid credentials")
-                }
-            )
-    }
+  def userLogin = Action.async {
+    implicit request =>
+      implicit val timeout: Timeout = Timeout(2 seconds)
+      FormsData.userForm.bindFromRequest().fold(
+        formWithErrors => Future.successful(BadRequest),
+        userTuple => {
+          loginRouter ? LoginActor.GetUser(userTuple._1, userTuple._2)
+        } map {
+          case Some(user) => Ok(views.html.loggedInPage(abc)).withSession("user" -> userTuple._1)
+          case None => Ok("Invalid credentials")
+        }
+      )
+  }
 
-       def demo = Action {
+  def demo = Action {
     Ok(views.html.demo())
-  }                         
+  }
 
   def createUser = Action.async {
     implicit request =>
@@ -96,9 +98,9 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents)(u
           val user1 = User(0, user.name, user.age, user.email, user.password)
           loginRouter ? LoginActor.CreateUser(user1)
         } map (someMes => someMes match {
-          case UserActionMessages.emailAlreadyExists => Ok(views.html.index(FormsData.userForm)(FormsData.createUserForm)("Email Id Already Exists"))
-          case UserActionMessages.genericError => Ok(views.html.index(FormsData.userForm)(FormsData.createUserForm)("Unable to create user. Please try again."))
-          case _ => Ok(views.html.index(FormsData.userForm)(FormsData.createUserForm)("User account created! Login to use our service"))
+          case UserActionMessages.emailAlreadyExists => Ok(html.index(FormsData.userForm)(FormsData.createUserForm)("Email Id Already Exists"))
+          case UserActionMessages.genericError => Ok(html.index(FormsData.userForm)(FormsData.createUserForm)("Unable to create user. Please try again."))
+          case _ => Ok(html.index(FormsData.userForm)(FormsData.createUserForm)("User account created! Login to use our service"))
         })
       )
 

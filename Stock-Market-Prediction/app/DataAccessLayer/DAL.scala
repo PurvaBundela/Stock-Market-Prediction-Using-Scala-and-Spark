14f757d588f337ec56b8e8e58a1d9e7120565baa
Dishank Shah <shah.di@husky.neu.edu>
2018-04-24 02:48:59 -0400

    Update DAL.scala

--- Stock-Market-Prediction/app/DataAccessLayer/DAL.scala
+++ Stock-Market-Prediction/app/DataAccessLayer/DAL.scala
@@ -6,10 +6,6 @@ import play.api.Play
 import play.api.db.slick.DatabaseConfigProvider
 import slick.driver.JdbcProfile
 
-/**
-  * Created by akashnagesh on 4/7/17.
-  */
-
 
 abstract class DAL(databaseConfig: DatabaseConfigProvider) {
   lazy val dbConfig = databaseConfig.get[JdbcProfile]
